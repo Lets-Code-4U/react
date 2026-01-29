66534e6836e08dab648b9faf7c53849920bbeb6a
Lets-Code-4U <sarveshletscode4u@gmail.com>
2026-01-29 15:07:28 +0530

    hello world project

---
+++ react-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+  <>               {/* this is a wrapper for all other components (empty tag) */}
+    <div id="parent">
+      <h1>Hello</h1>
+      <h2>Hello</h2>
+    </div>
+    <div id="parent2">
+      <h3>Hello</h3>
+      <h4>Hello</h4>
+    </div>
+  </>
+  )
+}
+
+export default App
